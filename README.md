40679edf926cdfcf2b1c23adeb34b65184899bdf
skytale75 <skytale75@yandex.com>
2020-05-19 12:11:25 -0700

    BrandNewSetupScript

--- README.md
+++ README.md
@@ -2,7 +2,8 @@
 
 Contact Information
 
-Facebook group for questions and answers is https://www.facebook.com/groups/666099570622915
+Facebook group for questions and answers is 
+https://www.facebook.com/groups/666099570622915
 
 My email is skytale75@yandex.com, if you have any issues
 installing this, please let me know, and paste any output
@@ -31,6 +32,11 @@ easy as possible for you to write your own file.
 Every graphical eliment on the main window has a help file.
 All you need to do to open it is press 'Controll + h'.
 
+***required packages***
+
+sudo apt install python3-tk
+sudo apt install git
+
 ***Download (Clone repository)***
 
 open terminal in home directory, copy/paste next line into
@@ -38,30 +44,15 @@ terminal and press enter.
 
 git clone https://github.com/skytale75/Conky_Tool.git
 
-note: if the terminal says git isn't installed use your systems
-package manager to install git. In apt you would type . . .
 
-sudo apt install git
 
 ***install***
 
-1) Navigate to the Conky_Tool folder in your terminal and run the
-the python script by copying the following 2 lines and executing
-one at a time.
+1) Copy and paste the following line into your terminal
+and press 'Enter'.
 
 
-***cd Conky_Tool***
+***sudo python3 ~/Conky_Tool/setup.py***
 
-***python3 setup.py***
 
-2) Change permissions on your launcher script to allow you to
-run it from anywhere in the terminal by copying the following
-2 lines and executing one at a time.
-
-***cd ~/bin***
-
-***chmod +x utilise***
-
-3) Type utilise into the terminal window to launch Utilise Conky.
-
-***utilise***
+2) Run, type 'utilise' in the terminal and press enter.
--- setup.py
+++ setup.py
@@ -2,25 +2,30 @@ import os, shutil
 from pathlib import Path
 
 home_path = os.path.expanduser("~/")
+bin_file_path = os.path.expanduser("~/bin")
 
 if "bin" in os.listdir(home_path):
-    pass
+    print("Found '~/bin' directory . . .")
 if "bin" not in os.listdir(home_path):
+    print("'~/bin' directory not found, creating ~/bin directory . . .")
     os.mkdir(home_path+"bin")
+    os.chmod(bin_file_path, 0o777)
 
 conky_path = os.path.expanduser("~/.config/")
 
 if "conky" in os.listdir(conky_path):
-    pass
+    print("Found 'conky' directory in '.config folder' . . .")
 if "conky" not in os.listdir(conky_path):
+    print("Creating 'conky' directory in '.config directory'")
     os.mkdir(conky_path+"conky")
 
 config_file = "conky.conf"
 config_path = os.path.expanduser("~/.config/conky/")
 
 if config_file in os.listdir(config_path):
-    pass
+    print("'conky.conf' file found . . .")
 if config_file not in os.listdir(config_path):
+    print("no 'conky.conf' file found, creating one in '~/.config/conky/'")
     copy_file = open(os.path.expanduser("~/Conky_Tool/Utilise_Conky/Conky_Themes/basic_example"), 'r')
     read_copy = copy_file.read()
     copy_file.close()
@@ -30,16 +35,19 @@ if config_file not in os.listdir(config_path):
 
 bin_path = os.path.expanduser("~/bin/")
 utilise = os.path.expanduser("~/Conky_Tool/utilise")
+user_utilise = os.path.expanduser("~/bin/utilise")
 
 if "utilise" in os.listdir(bin_path):
-    pass
+    print("'utlize' command found in ~/bin")
 if "utilise" not in os.listdir(bin_path):
+    print("'utilise' command not found in '~/bin', creating file . . .")
     old_u = open(utilise, 'r')
     read_u = old_u.read()
     new_u = open(bin_path+"utilise", 'w')
     new_u.write(read_u)
     old_u.close()
     new_u.close()
+    os.chmod(user_utilise, 0o777)
 
 utilise_path = os.path.expanduser("~/.config/Utilise_Conky/")
 conky_themes_path = os.path.expanduser(utilise_path+"/Conky_Themes/")
@@ -54,11 +62,15 @@ def the_copy(s_file):
     open_copy.close()
 
 if "Utilise_Conky" in os.listdir(conky_path):
-    pass
+    print("'Utilise_Conky' directory found in '~/.config' . . .")
 if "Utilise_Conky" not in os.listdir(conky_path):
+    print("'Utilise_Conky' directory not found in '~/.config', creating. . .")
     os.mkdir(os.path.expanduser("~/.config/"+"Utilise_Conky"))
     os.mkdir(utilise_path+"Conky_Themes")
     the_copy("basic_example")
     the_copy("new_transparent")
     the_copy(".fontlist.txt")
     the_copy(".themes.txt")
+
+print("\nscript completed . . . \n\nMake sure you have python3-tk installed . . .")
+print("sudo apt install python3-tk\nor whatever is appropriate for your distro")
